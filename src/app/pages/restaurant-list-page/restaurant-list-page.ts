70ab6a6787e16d57aeee81072f323181b1de04ad
TomasNanni <tomasnanni18@gmail.com>
2026-01-26 21:28:30 -0300

    primer commit

---
+++ src/app/pages/restaurant-list-page/restaurant-list-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-list-page',
+  imports: [],
+  templateUrl: './restaurant-list-page.html',
+  styleUrl: './restaurant-list-page.scss',
+})
+export class RestaurantListPage {
+
+}
