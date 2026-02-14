776328e66d61f0b0fcb313ceda5ad5a4a8a71cb9
TomasNanni <tomasnanni18@gmail.com>
2026-02-14 18:30:15 -0300

    terminado landing page

--- src/app/pages/landing-page/landing-page.ts
+++ src/app/pages/landing-page/landing-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TopBarLayout } from "../../layout/layout/top-bar-layout/top-bar-layout";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [TopBarLayout],
+  imports: [TopBarLayout, RouterLink],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
