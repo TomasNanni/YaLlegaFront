43ace6e79c98861fc3248f4a209a4002d9386313
TomasNanni <tomasnanni18@gmail.com>
2026-02-22 15:36:24 -0300

    import y newcategory

--- src/app/components/category-item/category-item.ts
+++ src/app/components/category-item/category-item.ts
@@ -1,12 +1,13 @@
 import { Component, input } from '@angular/core';
 import { Category } from '../../interfaces/category';
+import { ProductItem } from "../product-item/product-item";
 
 @Component({
   selector: 'app-category-item',
-  imports: [],
+  imports: [ProductItem],
   templateUrl: './category-item.html',
   styleUrl: './category-item.scss',
 })
 export class CategoryItem {
-category = input.required<Category>();
+  category = input.required<Category>();
 }
--- src/app/interfaces/category.ts
+++ src/app/interfaces/category.ts
@@ -1,8 +1,15 @@
-import { Product } from "../components/product/product";
+import { Product } from "./product"
+
 
 export interface Category {
     id : number,
     name : string,
     description : string,
     products : Product[]
+}
+export interface NewCategory{
+    name: string,
+    description: string,
+    RestaurantUserId: number,
+    productsId : number[];
 }
