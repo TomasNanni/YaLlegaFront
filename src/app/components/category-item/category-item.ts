edf1ba000464a7a07453766a90b1704f4962cacf
TomasNanni <tomasnanni18@gmail.com>
2026-06-29 15:02:06 -0300

    cambiado el estilo de categoria destacada

--- src/app/components/category-item/category-item.ts
+++ src/app/components/category-item/category-item.ts
@@ -1,4 +1,5 @@
 import { Component, input } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { Category } from '../../interfaces/category';
 import { ProductItem } from "../product-item/product-item";
 import { RouterLink } from '@angular/router';
@@ -6,7 +7,7 @@ import { NewProductItem } from "../new-product-item/new-product-item";
 
 @Component({
   selector: 'app-category-item',
-  imports: [ProductItem, RouterLink, NewProductItem],
+  imports: [ProductItem, RouterLink, NewProductItem, NgClass],
   templateUrl: './category-item.html',
   styleUrl: './category-item.scss',
 })
@@ -14,5 +15,6 @@ export class CategoryItem {
   category = input.required<Category>();
   isOwner = input.required<boolean>();
   idRestaurant = input.required<number>();
+  isStandout = input<boolean>(false);
 
 }
